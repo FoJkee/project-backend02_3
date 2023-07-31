daeecdc93884183d72d06ca66f386e8a9879df17
FoJkee <romanovsky0815@gmail.com>
2023-07-31 18:45:06 +0300

    fix26

--- src/routing/auth-routing.ts
+++ src/routing/auth-routing.ts
@@ -61,8 +61,8 @@ authRouter.post('/refresh-token', async (req: Request, res: Response) => {
         return res.sendStatus(401)
     }
 
-
     const newToken = await jwtService.createJwt(new ObjectId(userId.id))
+    if(!newToken) return res.sendStatus(401)
     res.cookie('refreshToken', newToken.refreshToken, {httpOnly: true, secure: true})
     return res.status(200).json({accessToken: newToken.accessToken})
 
