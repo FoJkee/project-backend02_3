195b52d33e6be45b82112c34956a5044d1e0ad47
FoJkee <romanovsky0815@gmail.com>
2023-08-23 18:45:31 +0300

    fix 25

--- src/domen/device-service.ts
+++ src/domen/device-service.ts
@@ -3,7 +3,6 @@ import {devicesCollection} from "../db";
 
 import {deviceRepo} from "../repository/device-repo";
 
-const pattern = "/\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z"
 
 export const deviceService = {
 
@@ -13,7 +12,7 @@ export const deviceService = {
             userId,
             ip,
             title,
-            lastActiveDate: pattern,
+            lastActiveDate: new Date().toDateString(),
             deviceId
         }
 
